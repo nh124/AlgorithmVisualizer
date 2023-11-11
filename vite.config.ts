de0454b3a25f7bd64ff2ae56aa9d3ae116a76285
Nur Haque <nur.haque99@gmail.com>
2023-11-10 22:33:23 -0500

    Updated workflows

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AlgorithmVisualizer/',
   plugins: [react()],
   test: {
     css: false,
